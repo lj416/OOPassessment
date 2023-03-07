7782e3009f1f13a36f5862a27e4030b8de76893a
ellejpacumbala <27210860@students.lincoln.ac.uk>
2023-03-07 18:04:21 +0000

    Initial commit

---
+++ cardProgram/Pack.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OOPassessment
+{
+    public class Pack
+    {
+        List<Card> pack;
+
+        public Pack()
+        {
+            //initialise the card pack here
+            
+        }
+
+        public static Card deal()
+        {
+            //deals one card
+            
+        }
+
+        public static bool shuffleCardPack(int typeOfShuffle)
+        {
+            //shuffles the card pack based on the type of shuffle
+            
+        }
+
+
+        public static List<Card> dealCard(int amount)
+        {
+            //deals the number of cards specified by amount
+          
+        }
+    }
+}
