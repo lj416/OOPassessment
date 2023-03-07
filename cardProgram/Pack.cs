2b33918143a955ad84d9a49d28269f2d9a1226b9
ellejpacumbala <27210860@students.lincoln.ac.uk>
2023-03-07 18:20:47 +0000

    modified:   cardProgram/Pack.cs Arrays for suit and value Added conditions for shuffle cards method

--- cardProgram/Pack.cs
+++ cardProgram/Pack.cs
@@ -8,10 +8,19 @@ namespace OOPassessment
     public class Pack
     {
         List<Card> pack;
-
+        Card[] deckOfCards;
         public Pack()
         {
-            //initialise the card pack here
+            int[] suitList = {1,2,3,4};
+            int[] valueList = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+
+            for (int i = 0; i < suitList.Length; i++)
+            {
+                for (int j = 0; j < valueList.Length;i++)
+                {
+
+                }
+            }
             
         }
 
@@ -24,7 +33,21 @@ namespace OOPassessment
         public static bool shuffleCardPack(int typeOfShuffle)
         {
             //shuffles the card pack based on the type of shuffle
-            
+            //1: Fisher-Yates Shuffle  2: Riffle Shuffle  3: No Shuffle
+            if (typeofShuffle = 1)
+            {
+
+            }
+            else if (typeOfShuffle = 2)
+            {
+
+            }
+            else if (typeOfShuffle = 3)
+            {
+
+            }
+            else
+                System.Console.WriteLine("Invalid.");
         }
 
 
